9e6fa8dfc4003a913a4f6d33a6d6f9a66e9ad9cc
rupaliwaghmareqa <rupaliqa2@gmail.com>
2026-06-20 17:17:48 +0530

    BrandFilter

--- playwright-croma/test-data/testdata.js
+++ playwright-croma/test-data/testdata.js
@@ -1,12 +1,17 @@
   export const testData =
- {
-  "pincode": "413304",
-  "search": {
-    "searchTerm": "refrigerator"
-  },
-  "brands": {
-    "brandFilter": ["Samsung", "LG"]
-  },
+ {  "pincode": "413304",
+ search: [
+     { searchTerm: "refrigerator" },
+    { searchTerm: "washing machine"},
+    { searchTerm: "microwave" },
+ ],
+
+  brands:[ 
+  { brandFilter:"Samsung"  },
+  { brandFilter:"LG" },
+  { brandFilter:"Apple" }
+],
+  
   "user": {
     "email": "test@example.com",
     "password": "TestPass@123"
